66e02db308c10d203fe9c8656a034a9223dc384f
Wadih Pazos <wadihjr@wpazos.com>
2024-03-19 20:05:03 -0400

    changed dataloader

--- training_data_loader.py
+++ training_data_loader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import numpy as np
+from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -14,9 +16,19 @@ class TrainingDataSet(Dataset):
         row = self.dataframe.iloc[index]
         # TODO: we can do further processing where it actually returns you image the way you want it
         image_name = row["img_filename"]
+        img = Image.open(
+            os.path.join(
+                "training_data/cpfs01/user/chengkanzhi/seeclick_web_imgs_part/",
+                image_name,
+            )
+        )
+        img = img.convert("RGB")
+        img = np.array(img)
+        # TODO: Probably should do resizing so batch in data loader is of same shape
+        # Tested, and it still was but as a precaution.
         instruction = row["instruction"]
         point = row["point"]
-        return image_name, instruction, point
+        return img, instruction, point
 
 
 def get_data_loader(batch_size=32):
@@ -24,3 +36,10 @@ def get_data_loader(batch_size=32):
     dataset = TrainingDataSet(df)
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return data_loader
+
+
+# data = get_data_loader()
+
+# for i, (img, instruction, point) in enumerate(data):
+#     print(img.shape)
+#     break
